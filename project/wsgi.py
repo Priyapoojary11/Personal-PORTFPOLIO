3ad51299921fefaa094f0d193a89d50787af8e77
Priya <44720654+Priyapoojary11@users.noreply.github.com>
2024-12-19 17:21:26 +0530

    Update wsgi.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+path = '/home/PriyaBirvas/Personal-PORTFPOLIO'
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
